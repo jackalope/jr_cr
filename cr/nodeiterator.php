715413ef419967286311e9daa3d06015fce84754
dbu <david.buchmann@liip.ch>
2009-07-03 10:08:32 +0200

    iterators

--- cr/nodeiterator.php
+++ cr/nodeiterator.php
@@ -1,131 +1,30 @@
 <?php
 
+class jr_cr_nodeiterator extends jr_cr_rangeiterator implements phpCR_NodeIterator {
 
+    protected $session = null;
 
-
-
-class jr_cr_nodeiterator  implements phpCR_NodeIterator{
-
-       protected $JRnodeiterator = null;
-    /**
-     *
-     */
     function __construct($jrnodeiterator,$session) {
+        parent::__construct($jrnodeiterator);
         $this->session = $session;
-        $this->JRnodeiterator = $jrnodeiterator;
     }
 
-    /**
-     *
-     * @return object
-     * @throws {@link NoSuchElementException}
-If iteration has no more {@link Node}s.
-     * @see phpCR_NodeIterator::nextNode()
-     */
-    public function nextNode() {
-        try {
-            $n = $this->JRnodeiterator->nextNode();
-        } catch(JavaException $e) {
-            $str = split("\n", $e->getMessage(), 1);
-            $str = $str[0];
-            if (strstr($str, 'NoSuchElementException')) {
-                throw new phpCR_NoSuchElementException($e->getMessage());
-            } else {
-                throw $e;
-            }
-        }
+    protected function createElement($n) {
         return new jr_cr_node($this->session,$n);
     }
 
-
-  /**
-   *
-   * @return int
-    * @see phpCR_RangeIterator::getPosition()
-*/
-  public  function getPosition() {
-      return $this->JRnodeiterator->getPosition();
-  }
-
-  /**
-   *
-   * @return int
-    * @see phpCR_RangeIterator::getSize()
-*/
-  public  function getSize() {
-
-    return $this->JRnodeiterator->getSize();
-
-  }
-
-  /**
-   *
-   * @param int
-The non-negative number of elements to skip
-   * @throws {@link NoSuchElementException}
-If skipped past the last element in the iterator.
-    * @see phpCR_RangeIterator::skip()
-*/
-  public  function skip($skipNum) {
-        try {
-            $this->JRnodeiterator->skip($skipNum);
-        } catch(JavaException $e) {
-            $str = split("\n", $e->getMessage(), 1);
-            $str = $str[0];
-            if (strstr($str, 'NoSuchElementException')) {
-                throw new phpCR_NoSuchElementException($e->getMessage());
-            } else {
-                throw $e;
-            }
-        }
-  }
-
-  /**
-   *
-    * @see Iterator::current()
-*/
-  public  function current() {
-
-      //TODO - Insert your code here
-      //FIXME: this is php only. java iterator can not do this
-  }
-
-  /**
-   *
-    * @see Iterator::key()
-*/
-  public  function key() {
-
-      //TODO - Insert your code here
-      //FIXME: this is php only. java iterator can not do this
-  }
-
     /**
-     *
-     * @see Iterator::next()
+     * @return object
+     * @throws {@link NoSuchElementException} If iteration has no more {@link Node}s.
+     * @see phpCR_NodeIterator::nextNode()
      */
-    public function next() {
-        return $this->nextNode();
+    public function nextNode() {
+        $this->next();
+        if ($this->valid()) {
+            return $this->current();
+        } else {
+            throw new phpCR_NoSuchElementException('nextNode called after end of iterator');
+        }
     }
-
-  /**
-   *
-    * @see Iterator::rewind()
-*/
-  public  function rewind() {
-
-      //TODO - Insert your code here
-      //FIXME: this is php only. java iterator can not do this
-  }
-
-  /**
-   *
-    * @see Iterator::valid()
-*/
-  public  function valid() {
-
-      //TODO - Insert your code here
-      //FIXME: this is php only. java iterator can not do this
-  }
 }
 
--- cr/rowiterator.php
+++ cr/rowiterator.php
@@ -1,120 +1,32 @@
 <?php
 
 
-class jr_cr_rowiterator implements phpCR_RowIterator {
+class jr_cr_rowiterator extends jr_cr_rangeiterator implements phpCR_RowIterator {
 
+    protected $session = null;
 
-    protected $JRrowiterator = null;
-    /**
-     *
-     */
     function __construct($jrrowiterator,$session) {
+        parent::__construct($jrrowiterator);
         $this->session = $session;
-        $this->JRrowiterator = $jrrowiterator;
     }
 
+    protected function createElement($r) {
+        return new jr_cr_row($r);
+    }
 
     /**
      *
-     * @return object
-A {@link Row} object
-     * @throws {@link NoSuchElementException}
-If iteration has no more {@link Row}s.
+     * @return object A {@link Row} object
+     * @throws {@link NoSuchElementException} If iteration has no more {@link Row}s.
      * @see phpCR_RowIterator::nextRow()
      */
     public function nextRow() {
-        try {
-            $n = $this->JRrowiterator->nextRow();
-        } catch(JavaException $e) {
-            $str = split("\n", $e->getMessage(), 1);
-            $str = $str[0];
-            if (strstr($str, 'NoSuchElementException')) {
-                throw new phpCR_NoSuchElementException($e->getMessage());
-            } else {
-                throw $e;
-            }
+        $this->next();
+        if ($this->valid()) {
+            return $this->current();
+        } else {
+            throw new phpCR_NoSuchElementException('nextRow called after end of iterator');
         }
-        return new jr_cr_row($n);
-    }
-
-    /**
-     *
-     * @return int
-     * @see phpCR_RangeIterator::getPosition()
-     */
-    public function getPosition() {
-        return $this->JRrowiterator->getPosition();
-    }
-
-    /**
-     *
-     * @return int
-     * @see phpCR_RangeIterator::getSize()
-     */
-    public function getSize() {
-        return $this->JRrowiterator->getSize();
-    }
-
-    /**
-     *
-     * @param int
-The non-negative number of elements to skip
-     * @throws {@link NoSuchElementException}
-If skipped past the last element in the iterator.
-     * @see phpCR_RangeIterator::skip()
-     */
-    public function skip($skipNum) {
-
-    //TODO - Insert your code here
-      //FIXME: this is php only. java iterator can not do this
-    }
-
-    /**
-     *
-     * @see Iterator::current()
-     */
-    public function current() {
-
-    //TODO - Insert your code here
-      //FIXME: this is php only. java iterator can not do this
-    }
-
-    /**
-     *
-     * @see Iterator::key()
-     */
-    public function key() {
-
-    //TODO - Insert your code here
-      //FIXME: this is php only. java iterator can not do this
-    }
-
-    /**
-     *
-     * @see Iterator::next()
-     */
-    public function next() {
-        return $this->nextRow();
-    }
-
-    /**
-     *
-     * @see Iterator::rewind()
-     */
-    public function rewind() {
-
-    //TODO - Insert your code here
-      //FIXME: this is php only. java iterator can not do this
-    }
-
-    /**
-     *
-     * @see Iterator::valid()
-     */
-    public function valid() {
-
-    //TODO - Insert your code here
-      //FIXME: this is php only. java iterator can not do this
     }
 }
 
